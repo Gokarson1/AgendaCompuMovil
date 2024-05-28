c899c657218fccbfd2424005b0d6c9535bb5081e
am4rv <ariosf@utem.cl>
2024-05-27 23:16:13 -0400

    olaaa

--- lib/Pages/profile.dart
+++ lib/Pages/profile.dart
@@ -1,9 +1,5 @@
-//aqui dejo el perfil de momento :V
-
-
 import 'package:agenda_compumovil/Pages/Welcome.dart';
 import 'package:agenda_compumovil/Services/Firebase.dart';
-import 'package:agenda_compumovil/Widget/Barra.dart';
 import 'package:agenda_compumovil/Widget/barra_inf.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -19,32 +15,30 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-appBar: const MiBarra(titulo: "Perfil =w="),
-
-
-body: Center(
-  child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Image.network(FirebaseAuth.instance.currentUser!.photoURL!),
-      Text("${FirebaseAuth.instance.currentUser!.displayName}"),
-      Text("${FirebaseAuth.instance.currentUser!.email}"),
-      ElevatedButton(
-        onPressed: ()async{
-        await FirebaseServices().signOut();
-        Navigator.push(context);,
-        MaterialPageRoute(builder: (context)=> const Home()));
-      
-      },
-      child: const Text("Logout"),
+      appBar: AppBar(
+        title: const Text("Perfil =w="),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(FirebaseAuth.instance.currentUser!.photoURL!),
+            Text("${FirebaseAuth.instance.currentUser!.displayName}"),
+            Text("${FirebaseAuth.instance.currentUser!.email}"),
+            ElevatedButton(
+              onPressed: () async {
+                await FirebaseServices().signOut();
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Home()),
+                );
+              },
+              child: const Text("Logout"),
+            ),
+          ],
         ),
-    ],
-  ),
-
-  ),
+      ),
       bottomNavigationBar: const BarraInf(),
-    
     );
   }
 }
